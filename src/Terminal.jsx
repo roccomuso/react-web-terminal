47b4ef24ca53cf1465c428c2ad060fb07d78f4ac
rocco <rocco@hackerstribe.com>
2017-06-29 17:04:38 +0200

    main commit

---
+++ src/Terminal.jsx
@@ -0,0 +1,140 @@
+import React, {Component} from 'react'
+import {Scrollbars} from 'react-custom-scrollbars'
+import './App.css'
+
+class Terminal extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      commands: {},
+      history: [],
+      prompt: '$ '
+    }
+
+    /* bind Methods to the Component scope */
+    this.clearHistory = this.clearHistory.bind(this)
+    this.registerCommands = this.registerCommands.bind(this)
+    this.listFiles = this.listFiles.bind(this)
+    this.showWelcomeMsg = this.showWelcomeMsg.bind(this)
+    this.showHelp = this.showHelp.bind(this)
+  }
+
+  clearHistory() {
+    this.setState({history: []});
+  }
+
+  registerCommands() {
+    this.setState({
+      commands: {
+        'clear': this.clearHistory,
+        'ls': this.listFiles,
+        'intro': this.showWelcomeMsg,
+        'help': this.showHelp,
+        'github': this.openLink('https://github.com/roccomuso')
+      }
+    });
+  }
+
+  listFiles() {
+    this.addHistory("README.md");
+  }
+
+  showWelcomeMsg() {
+    this.addHistory("Hello, this web-terminal lets you take control of a remote device.");
+    this.addHistory("Type `help` to see what all commands are available");
+  }
+
+  openLink(link) {
+    return function() {
+      window.open(link, '_blank');
+    }
+  }
+
+  showHelp() {
+    this.addHistory([
+      "help - this help text",
+      "github - view my github profile",
+      "source - browse the code for this page",
+      "intro - print intro message",
+      "blog - read some stuff that I've written",
+      "clear - clear screen",
+      "ls - list files",
+      "resume - view my resume"
+    ])
+  }
+
+  componentDidMount() {
+    var term = this.term
+
+    this.registerCommands();
+    this.showWelcomeMsg();
+    term.focus();
+  }
+
+  componentDidUpdate() {
+    this.scrollBar.scrollToBottom()
+  }
+
+  handleInput(e) {
+    if (e.key === "Enter") {
+      var input_text = this.term.value;
+      var input_array = input_text.split(' ');
+      var input = input_array[0];
+      var arg = input_array[1];
+      var command = this.state.commands[input];
+
+      this.addHistory(this.state.prompt + " " + input_text);
+
+      if (command === undefined) {
+        this.addHistory("sh: command not found: " + input);
+      } else {
+        command(arg);
+      }
+      this.clearInput();
+    }
+  }
+
+  clearInput() {
+    this.term.value = "";
+  }
+
+  addHistory(output) {/* output: Array or String */
+    var history = this.state.history;
+    if (Array.isArray(output)) {
+      history = history.concat(output)
+    } else {
+      history.push(output)
+    }
+
+    this.setState({'history': history})
+  }
+
+  handleClick() {
+    var term = this.term
+    term.focus()
+  }
+
+  render() {
+    var output = this.state.history.map(function(op, i) {
+      return <p key={i}>{op}</p>
+    })
+
+    return (
+      <Scrollbars style={{ width: 715 }} autoHeight autoHeightMin={100} autoHeightMax={400} ref={elem => this.scrollBar = elem} autoHide autoHideTimeout={1000} autoHideDuration={200}>
+        <div id="content">
+          <div className='input-area' onClick={this.handleClick.bind(this)}>
+            {output}
+            <p>
+              <span className="prompt">{this.state.prompt}</span>
+              <input type="text" onKeyPress={this.handleInput.bind(this)} ref={elem => this.term = elem}/>
+            </p>
+          </div>
+        </div>
+      </Scrollbars>
+    )
+  }
+
+}
+
+export default Terminal
